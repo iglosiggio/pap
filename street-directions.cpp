80e85a2401dc9486792cc6107f3a399e68641423
Ignacio Losiggio <iglosiggio@gmail.com>
2019-10-01 18:54:13 -0300

    Street directions da accepted

--- street-directions.cpp
+++ street-directions.cpp
@@ -1,52 +1,32 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <stack>
+#include <set>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::map;
 using std::vector;
-using std::stack;
+using std::set;
 using std::pair;
 using std::min;
 using G = map<int, vector<int>>;
 
 struct Data {
-	stack<pair<int, int>> s;
 	vector<int> depth;
 	vector<int> low;
-	vector<int> articulaciones;
-	vector<pair<int, int>> puentes;
-	vector<vector<pair<int, int>>> componentes;
+	set<pair<int, int>> ejes;
 
 	Data(int n) : depth(n + 1, -1), low(n + 1) {}
 };
 
-void reportarComponente(int v, int w, Data& data) {
-	stack<pair<int, int>>& stack = data.s;
-		
-	data.componentes.emplace_back();
-	vector<pair<int, int>>& componente = data.componentes.back();
-
-	componente.emplace_back(stack.top());
-
-	while (stack.top() != pair<int, int>{v, w}) {
-		stack.pop();
-		componente.emplace_back(stack.top());
-	}
-	stack.pop();
-}
-
 int dfs(const G& g, int v, int d, int padre, Data& data) {
+	set<pair<int, int>>& ejes = data.ejes;
 	int& depth = data.depth[v];
 	int& low = data.low[v];
 	depth = d;
 	low = d;
-
-	if (v != padre)
-		data.s.emplace(padre, v);
 
 	for (auto w : g.at(v))
 	if (w != padre) {
@@ -55,33 +35,23 @@ int dfs(const G& g, int v, int d, int padre, Data& data) {
 
 		if (w_depth == -1) {
 			low = min(low, dfs(g, w, d + 1, v, data));
-			if (w_low >= depth) {
-				data.articulaciones.push_back(v);
-				reportarComponente(v, w, data);
-			}
+			ejes.emplace(v, w);
 			if (w_low >= w_depth)
-				data.puentes.emplace_back(v, w);
-		} else
+				ejes.emplace(w, v);
+		} else {
 			low = min(low, w_depth);
+			if (ejes.find(pair<int, int>{w, v}) == ejes.end())
+				ejes.emplace(v, w);
+		}
+
 	}
 
 	return low;
 }
 
-Data biconexas(const G& g) {
-	Data data(g.size());
-
-	dfs(g, 1, 0, 1, data);
-
-	return data;
-}
-
-void resolver() {
-	int n, m;
+void resolver(int n, int m) {
 	G g;
 	vector<pair<int, int>> e;
-
-	cin >> n >> m;
 
 	for (int i = 0; i < m; i++) {
 		pair<int, int> eje;
@@ -92,40 +62,21 @@ void resolver() {
 		g[eje.second].push_back(eje.first);
 	}
 
-	/* La entrada termina con un 0 0 */
-	cin >> n >> m;
+	Data data(n);
+	dfs(g, 1, 0, 1, data);
 
-	Data d = biconexas(g);
-
-	cout << "Componentes!" << endl;
-	cout << "==================" << endl;
-	int cid = 0;
-	for (const auto& componente : d.componentes) {
-		cout << "Componente " << cid << endl;
-		cout << "==================" << endl;
-		for (const auto& eje : componente)
-			cout << eje.first << ' ' << eje.second << endl;
-		cid++;
-	}
-	cout << "Puentes!" << endl;
-	cout << "==================" << endl;
-	for (const auto& puente : d.puentes) {
-		cout << puente.first << ' ' << puente.second << endl;
-	}
-	cout << "Articulaciones!" << endl;
-	cout << "==================" << endl;
-	for (const auto& art : d.articulaciones) {
-		cout << art << endl;
-	}
+	for (const auto& eje : data.ejes)
+		cout << eje.first << ' ' << eje.second << endl;
 }
 
 int main(int argc, char* argv[]) {
-	int t;
-	cin >> t;
-	for (int i = 1; i <= t; i++) {
-		cout << i << endl;
-		resolver();
+	int i = 0, n, m;
+
+	while (i++, cin >> n >> m, n || m) {
+		cout << i << endl << endl;
+		resolver(n, m);
 		cout << '#' << endl;
 	}
+
 	return 0;
 }
